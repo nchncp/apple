cb4a615b7b3994e2364be6f20e51b10391cca0f0
Nichapa Pakhakorntanatorn <nchncp@gmail.com>
2017-08-29 22:31:37 +0700

    Nav, Slide and Div

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+  $('.fadeOut').owlCarousel({
+    items: 1,
+    center: true,
+    animateOut: 'fadeOutLeft',
+    // nav: true,
+    dots: false,
+    loop: true,
+    margin: 10,
+  });
+});
